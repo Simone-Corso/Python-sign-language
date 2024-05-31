5835e919d090922ae35c10bec93d3dbc7ea6ea93
Simone-Corso <scorso26@gmail.com>
2024-05-31 15:48:56 +0200

    ho inserito istanza per rivelamengo delle mani e poi cattura di video

--- sign.py
+++ sign.py
@@ -2,3 +2,15 @@
 
 import mediapipe as mp
 import cv2
+
+
+# configuriamo istanza per rivelamento delle mani
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
+mp_drawing = mp.solutions.drawing_utils
+
+
+# catturiamo video utilizzando OpenCv
+
+cap = cv2.VideoCapture(0)
