97672d09c6066907c272f5ea9b8743ccbc10f3ba
Simone-Corso <scorso26@gmail.com>
2024-05-31 16:08:02 +0200

    funziona perfettamente la parte per salvare il gesto realizzato

--- save_gesture.py
+++ save_gesture.py
@@ -8,7 +8,7 @@ hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_c
 mp_drawing = mp.solutions.drawing_utils
 
 # Avvia la cattura video
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 # Dizionario per memorizzare i punti di riferimento dei gesti
 gestures = {}
